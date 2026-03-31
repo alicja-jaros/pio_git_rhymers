4bc9333ae45c78c3c8b0a0243e23613ac732e580
alicjajaros <255613@edu.p.lodz.pl>
2026-03-31 12:54:00 +0200

    17. Dodano dokumentacje klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Implementacja stosu przechowującego liczby całkowite w kolejności LIFO
+ */
 public class DefaultCountingOutRhymer {
 
     public static final int RHYMER_CAPACITY = 12;
@@ -11,32 +14,50 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY_RHYMER;
 
-
+    /**
+     * Sprawdza czy struktura nie jest pełna, jeśli nie to dodaje element do struktury
+     * @param in wartość do dodania
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
-
+    /**
+     * Sprawdza czy struktura jest pusta.
+     * @return true jeśli pusta, false jesli nie
+     */
     public boolean callCheck() {
         return total == EMPTY_RHYMER;
     }
-
+    /**
+     * Sprawdza czy struktura jest pełna
+     * @return true jeśli pełna, false jeśli nie
+     */
     public boolean isFull() {
         return total == (RHYMER_CAPACITY - 1);
     }
-
+    /**
+     * Zwraca ostatni dodany element ale go nie usuwa
+     * @return Jeśli struktura jest pusta to domyślna wartośc, inaczej ostatnia wartość
+     */
     protected int peekaboo() {
         if (callCheck())
             return DEFAULT_VALUE;
         return numbers[total];
     }
-
+    /**
+     * Usuwa i zwraca ostatni dodany element.
+     * @return Jeśli struktura jest pusta to domyślna wartośc, inaczej ostatnia wartość
+     */
     public int countOut() {
         if (callCheck())
             return DEFAULT_VALUE;
         return numbers[total--];
     }
-
+    /**
+     * Zwraca indeks ostatniego elementu w strukturze
+     * @return indeks ostatniego elementu lub -1 jeśli struktura jest pusta
+     */
     public int getTotal() {
         return total;
     }
